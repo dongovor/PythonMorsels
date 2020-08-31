6f20e722b93b4bdd504d22eebe1dd1fcc7941a16
Dmytro Cherkasov <dcherkasov@deloitte.com>
2020-08-31 19:00:11 +0300

    rollback

--- add/main.py
+++ add/main.py
@@ -1,5 +1,3 @@
-#add project
-'''
 def add(in_matrix_1, in_matrix_2):
     out_matrix = []
     for i in range(len(in_matrix_1)):
@@ -8,21 +6,3 @@ def add(in_matrix_1, in_matrix_2):
             row.append(in_matrix_1[i][j] + in_matrix_2[i][j])
         out_matrix.append(row)
     return out_matrix
-'''
-def add(*in_args):
-    for i in (*in_args):
-        print('-----')
-        print(i)
-        print('-----')
-    return 
-
-
-in_1 = [[1, -2], [-3, 4]]
-in_2 = [[2, -1], [0, -1]]
-
-print(add(in_1, in_2))
-
-in_1 = [[1, -2, 3], [-4, 5, -6], [7, -8, 9]]
-in_2 = [[1, 1, 0], [1, -2, 3], [-2, 2, -2]]
-
-print(add(in_1, in_2))

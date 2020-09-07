e37cf5ed5a2ae28cbeb8d2d0dbb331350cbfe63f
Dmytro Cherkasov <dcherkasov@deloitte.com>
2020-09-07 14:29:53 +0300

    upd

--- semantic_wrap/main.py
+++ semantic_wrap/main.py
@@ -1,5 +1,6 @@
 def semantic_wrap(in_str):
     PUNKT_CHAR = ('.', '?', '!')
     for p_char in PUNKT_CHAR:
+        in_str = in_str.replace(f'{p_char}  ', f'{p_char}\n')
         in_str = in_str.replace(f'{p_char} ', f'{p_char}\n')
     return in_str

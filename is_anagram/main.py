1f02ae109e5141cd13087f6823fb03e97df1d788
Dmytro Cherkasov <dcherkasov@deloitte.com>
2020-09-10 14:53:52 +0300

    code shortened

--- is_anagram/main.py
+++ is_anagram/main.py
@@ -2,10 +2,5 @@ import string
 
 def is_anagram(in_str1, in_str2):
     in_str1 = in_str1.translate(str.maketrans('', '', string.punctuation)).replace(' ','')
-    in_str2 = in_str2.translate(str.maketrans('', '', string.punctuation)).replace(' ','')
-    if sorted(in_str1.lower().strip()) == sorted(in_str2.lower().strip()):
-        return True
-    else:
-        return False
-
-print(is_anagram("cardiografía", "radiográfica"))
+    in_str2 = in_str2.translate(str.maketrans('', '', string.punctuation)).replace(' ','')  
+    return sorted(in_str1.lower()) == sorted(in_str2.lower())

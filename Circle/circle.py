d182336e4768dc69e165365420a2efb9c35b1fe7
Dmytro Cherkasov <dcherkasov@deloitte.com>
2020-09-16 17:43:42 +0300

    global upd w base and 3 bonuses

--- Circle/circle.py
+++ Circle/circle.py
@@ -1,19 +1,31 @@
 import math
 
-class Circle:
+class Circle():
+    
     def __init__(self, radius = 1):
         self.radius = radius
 
+    def __repr__(self):
+        return f"Circle({self.radius})"
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, radius):
+        if radius < 0:
+            raise ValueError("Radius cannot be negative")
+        self._radius = radius
+
+    @property
     def area(self):
-        return (math.pi) * (self.radius) ** 2
+        return math.pi * self.radius ** 2
 
+    @property
     def diameter(self):
-        return str(2 * self.radius)
-
-
-c = Circle()
-print(c.diameter())
-
-c.radius = 5
-
-print(c.diameter())
+        return 2 * self.radius
+                
+    @diameter.setter
+    def diameter(self, diameter):
+        self.radius = diameter / 2

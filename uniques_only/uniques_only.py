3e8cb30fa869e468ecc48853b30b00680a20adba
Dmytro Cherkasov <dcherkasov@deloitte.com>
2020-10-12 18:02:08 +0300

    upd

--- uniques_only/uniques_only.py
+++ uniques_only/uniques_only.py
@@ -1,8 +1,11 @@
 def uniques_only(in_iter):
-    tmp_lst = []
-    for i, j in enumerate(in_iter):
-        if j in tmp_lst:
-            pass
-        else:
-            tmp_lst.append(j)
-    return tmp_lst
+    tmp_set = set()
+    for item in in_iter:
+        if item not in tmp_set:
+            yield item
+            tmp_set.add(item)
+
+
+nums = iter([1, 2, 3])
+output = uniques_only(nums)
+print(output)
